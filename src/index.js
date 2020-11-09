9afd25f8a4cf672eafca7687e248a71063fb1b14
Myron <dubtwee@gmail.com>
2020-11-09 10:20:08 -0600

    setting up redux with react app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// REDUCERS
+const defaultFeedback = {
+  feeling: '',
+  understanding: '',
+  support: '',
+  comments: '',
+};
+function feedback(state = defaultFeedback, action) {
+  return state;
+}
+
+// create store instance
+const storeInstance = createStore(
+  combineReducers({
+    feedback,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
